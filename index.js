44299a1922f0e8a145c55c9c60c71697c351139b
dennissammy77 <dennissammy77@gmail.com>
2025-03-04 16:18:06 +0300

    Controll lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if(depth <= 400){
+    return 'This one is on me!'
+  }else if(depth >= 400 && depth <= 1500){
+    return 'That will be twenty bucks.'
+  }else if(depth > 2000 && depth <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(depth > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(character){
   // Write your code here!
+  switch (character) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
